526b1d47e6bb08659e25dc4263bc467ec913314d
Facundo Fernandez <facuassain@gmail.com>
2022-06-05 11:53:01 -0300

    Script checks added

--- install-commons.sh
+++ install-commons.sh
@@ -5,8 +5,8 @@ rm -rf so-commons-library
 git clone https://github.com/sisoputnfrba/so-commons-library
 
 if cd so-commons-library && make install; then
-    rm -rf so-commons-library
-    echo "\033[1;32m so-commons-library installed successfully\033[0m"
+    cd && rm -rf so-commons-library install-commons.sh
+    echo -e "\033[1;32m so-commons-library installed successfully\033[0m"
 else
-    echo "\033[1;31m so-commons-library installation failed\033[0m"
+    echo -e "\033[1;31m so-commons-library installation failed\033[0m"
 fi
